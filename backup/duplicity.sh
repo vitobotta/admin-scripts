c1c10664aa0d0683b94290d45f7cace97d19126d
Vito Botta <vito@botta.name>
2012-08-04 14:40:38 +0300

    Added support for running scripts before and after the backup

--- backup/duplicity.sh
+++ backup/duplicity.sh
@@ -12,7 +12,7 @@ DUPLICITY=$(which duplicity)
 CONFIG_FILE=$HOME/.duplicity.config
 
 if [ -f $CONFIG_FILE ]; then
-	echo "Loading configuration from $CONFIG_FILE."
+	echo -e "Loading configuration from $CONFIG_FILE."
 	source $CONFIG_FILE
 else
 cat << EOF > $CONFIG_FILE
@@ -20,15 +20,21 @@ BACKUP_SOURCE_DIRECTORIES=(/home /root /var/www /var/log /etc /usr/local)
 BACKUP_USER="$(whoami)"
 BACKUP_HOST=
 BACKUP_TARGET_DIRECTORY="duplicity/"
-KEEP_FULL_BACKUPS=8
+MAX_FULL_BACKUPS=8
 MAX_INCREMENTALS_AGE=1W
 MAX_CHAIN_AGE=2M
 MAX_VOLUME_SIZE=250
 VERBOSITY=4 # 0 is total silent, 4 is the default, and 9 is noisiest
 DUPLICITY_PASSPHRASE=
+
+# Comment out the following if you want to run one or more scripts before duplicity backup.
+#RUN_BEFORE=(/some/script /another/script)
+
+# Comment out the following if you want to run one or more scripts after duplicity backup.
+#RUN_AFTER=(/some/script /another/script)
 EOF
 
-	die "Configuration has been initialised in $CONFIG_FILE. Please make sure all settings are correctly defined/customised - aborting."
+	die "Configuration has been initialised in $CONFIG_FILE. \nPlease make sure all settings are correctly defined/customised - aborting."
 fi
 
 (
@@ -50,6 +56,19 @@ INCLUDE="$(for s in ${BACKUP_SOURCE_DIRECTORIES[@]} ; do echo --include=$s; done
 DUPLICITY_SETTINGS="--verbosity=${VERBOSITY-warning} --archive=/tmp/duplicity --allow-source-mismatch --volsize=$MAX_VOLUME_SIZE"
 DUPLICITY_COMMAND="$(which nice) -n 15 $IONICE_COMMAND duplicity $DUPLICITY_SETTINGS"
 
+if [ -n "$BACKUP_USER" ]; then
+	echo -e "Running 'before' scripts...\n"
+	for SCRIPT in ${RUN_BEFORE[@]}; do
+		if [ -f $SCRIPT ]; then
+			$SCRIPT
+		else
+			echo "WARNING: before script $SCRIPT not found"
+		fi
+	done
+	echo
+fi
+
+
 ssh $BACKUP_USER@$BACKUP_HOST mkdir -vp $BACKUP_TARGET_DIRECTORY
 
 ( 
@@ -60,10 +79,10 @@ ssh $BACKUP_USER@$BACKUP_HOST mkdir -vp $BACKUP_TARGET_DIRECTORY
   elif [ "$1" = "incr" ]; then
   	$DUPLICITY_COMMAND incr --full-if-older-than=$MAX_INCREMENTALS_AGE $INCLUDE --exclude='**' --asynchronous-upload / $TARGET
   else
-    die "Periodicity not specified: Please run: as $0 [incr|full]"
+    die "Periodicity not specified. Please run: as $0 [incr|full]"
   fi
 
-	$DUPLICITY_COMMAND remove-all-but-n-full $KEEP_FULL_BACKUPS $TARGET --force
+	$DUPLICITY_COMMAND remove-all-but-n-full $MAX_FULL_BACKUPS $TARGET --force
 	$DUPLICITY_COMMAND remove-older-than $MAX_CHAIN_AGE $TARGET --force
 	$DUPLICITY_COMMAND cleanup --extra-clean --force $TARGET
 	$DUPLICITY_COMMAND collection-status $TARGET
@@ -71,3 +90,15 @@ ssh $BACKUP_USER@$BACKUP_HOST mkdir -vp $BACKUP_TARGET_DIRECTORY
 	unset DUPLICITY_PASSPHRASE
 	unset PASSPHRASE
 )
+
+if [ -n "$BACKUP_USER" ]; then
+	echo -e "Running 'after' scripts...\n"
+	for SCRIPT in ${RUN_AFTER[@]}; do
+		if [ -f $SCRIPT ]; then
+			$SCRIPT
+		else
+			echo "WARNING: before script $SCRIPT not found"
+		fi
+	done
+	echo
+fi
