26d9addb464623ee0cb0f8dcdfe151a4d1475a52
Vito Botta <vito@botta.name>
2012-08-05 22:43:18 +0300

    duplicity: added possibility to chose, in the configuration, between pub/key or symmetric encryption

--- backup/duplicity.sh
+++ backup/duplicity.sh
@@ -8,6 +8,9 @@ die () {
 DUPLICITY=$(which duplicity)
 [ -f "$DUPLICITY" ] || die "Duplicity not found - please ensure it is installed before proceeding."
 
+ENCRYPTION=1
+COMPRESSION_LEVEL=6
+
 
 CONFIG_FILE=$HOME/.duplicity.config
 
@@ -28,8 +31,20 @@ MAX_FULL_BACKUPS_TO_RETAIN=8
 MAX_AGE_INCREMENTALS_TO_RETAIN=1W
 MAX_AGE_CHAINS_TO_RETAIN=2M
 MAX_VOLUME_SIZE=250
-VERBOSITY=4 # 0 is total silent, 4 is the default, and 9 is noisiest
-DUPLICITY_PASSPHRASE=
+
+ENCRYPTION=1
+PASSPHRASE= # used for ENCRYPT_KEY or, if this is not specified, for symmetric encryption
+
+# Set ENCRYPT_KEY if you want to use GPG pub key encryption. Otherwise duplicity will just use symmetric encryption. 
+# ENCRYPT_KEY=
+
+# Optionally use a different key for signing
+# SIGN_KEY=
+# SIGN_KEY_PASSPHRASE=
+
+COMPRESSION_LEVEL=6 # 0 disables compression
+
+VERBOSITY=4 # 0 Error, 2 Warning, 4 Notice (default), 8 Info, 9 Debug (noisiest)
 
 # Comment out the following if you want to run one or more scripts before duplicity backup.
 #RUN_BEFORE=(/some/script /another/script)
@@ -43,7 +58,6 @@ fi
 
 [ ${#INCLUDE[@]} -gt 0 ] || die "Please set which directories you want to backup (setting 'INCLUDE' in the configuration file)."
 [ -n "$TARGET" ] || die "Please set the destination directory that will contain your backups (setting 'TARGET' in the configuration file)."
-[ -n "$DUPLICITY_PASSPHRASE" ] || die "Please set the passphrase Duplicity requires for GPG encryption (setting 'DUPLICITY_PASSPHRASE' in the configuration file)."
 
 IONICE=$(which ionice)
 
@@ -52,7 +66,20 @@ if [ -n "$IONICE" ]; then
 fi
 
 INCLUDE="$(for s in ${INCLUDE[@]} ; do echo --include=$s; done)"
-DUPLICITY_SETTINGS="--verbosity=${VERBOSITY-warning} --archive=/tmp/duplicity --allow-source-mismatch --volsize=$MAX_VOLUME_SIZE"
+
+DUPLICITY_SETTINGS="--verbosity=${VERBOSITY-warning} --allow-source-mismatch --volsize=$MAX_VOLUME_SIZE"
+
+if [[ $ENCRYPTION -eq 1 ]]; then
+	[ -n "$ENCRYPT_KEY" ] && DUPLICITY_SETTINGS="$DUPLICITY_SETTINGS --encrypt-key=$ENCRYPT_KEY"
+	[ -n "$SIGN_KEY" ] && DUPLICITY_SETTINGS="$DUPLICITY_SETTINGS --sign-key=$SIGN_KEY"
+	# --gpg-options='--compress-algo=bzip2'
+	#  --gpg-options='--compress-algo=bzip2'
+else
+	# export GZIP="-1"
+	DUPLICITY_SETTINGS="$DUPLICITY_SETTINGS --no-encryption"
+fi
+
+
 DUPLICITY_COMMAND="$(which nice) -n 15 $IONICE_COMMAND duplicity $DUPLICITY_SETTINGS"
 
 if [ ${#RUN_BEFORE[@]} -gt 0 ]; then
@@ -69,7 +96,8 @@ fi
 
 
 ( 
-  export PASSPHRASE=$DUPLICITY_PASSPHRASE
+  [ -n "$PASSPHRASE" ] && export PASSPHRASE=$PASSPHRASE
+	[ -n "$SIGN_KEY_PASSPHRASE" ] && export SIGN_PASSPHRASE=$SIGN_KEY_PASSPHRASE
 
   if [ "$1" = "full" ]; then
     $DUPLICITY_COMMAND full $INCLUDE --exclude='**' --asynchronous-upload / $TARGET
@@ -85,6 +113,7 @@ fi
 	$DUPLICITY_COMMAND collection-status $TARGET
 
 	unset DUPLICITY_PASSPHRASE
+	unset SIGN_PASSPHRASE
 	unset PASSPHRASE
 )
 
