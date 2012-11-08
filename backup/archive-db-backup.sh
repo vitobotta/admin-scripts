8d14bbafb0f35102d25812b5e3c723b0e5396981
Vito Botta <no@spam.com>
2012-11-08 12:09:46 +0200

    Speed up the download of production data by just downloading the latest archive already available on the selected node

--- backup/archive-db-backup.sh
+++ backup/archive-db-backup.sh
@@ -25,16 +25,20 @@ echo "Preparing copy of the latest backup available on $HOSTNAME..."
 LAST_BACKUP_TIMESTAMP=`find /backup/mysql/ -mindepth 2 -maxdepth 2 -type d -exec ls -dt {} \+ | head -1 | rev | cut -d '/' -f 1 | rev`
 DESTINATION="/backup/mysql/archives"
 ARCHIVE="$DESTINATION/production-data.$(date +%Y-%m-%d-%H.%M.%S).tgz"
+TEMPFILE=`tempfile`
 TEMP_DIRECTORY=`mktemp -d`
 
 mkdir -vp $DESTINATION
+[[ -f $TEMPFILE ]] && rm -rf $TEMPFILE
 
 /admin-scripts/backup/xtrabackup.sh restore $LAST_BACKUP_TIMESTAMP $TEMP_DIRECTORY
 
 echo "Prepared a copy of the data, now creating compressed archive $ARCHIVE..."
 
-/usr/bin/ionice -c2 -n7 tar cvfz $ARCHIVE $TEMP_DIRECTORY &> $LOG_FILE || fail
+/usr/bin/ionice -c2 -n7 tar cvfz $TEMPFILE $TEMP_DIRECTORY &> $LOG_FILE || fail
 /usr/bin/ionice -c2 -n7 rm -rf $TEMP_DIRECTORY &> $LOG_FILE || fail
+
+mv $TEMPFILE $ARCHIVE
 
 echo "Compressed archive created."
 
